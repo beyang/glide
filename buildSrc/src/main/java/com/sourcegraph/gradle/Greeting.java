9dc1c76f570ebd864b056ed5d9ff24fdcf1f7096
Beyang Liu <beyang@sourcegraph.com>
2018-05-07 15:13:43 -0700

    .

--- buildSrc/src/main/java/com/sourcegraph/gradle/Greeting.java
+++ buildSrc/src/main/java/com/sourcegraph/gradle/Greeting.java
@@ -2,19 +2,47 @@ package com.sourcegraph.gradle;
 
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.Project;
 
 public class Greeting extends DefaultTask {
-    private String message;
-    private String recipient;
+    /**
+     * The root project
+     */
+    private Project project;
 
-    public String getMessage() { return message; }
-    public void setMessage(String message) { this.message = message; }
+    public Project getProject() {
+        return project;
+    }
 
-    public String getRecipient() { return recipient; }
-    public void setRecipient(String recipient) { this.recipient = recipient; }
+    public void setProject(Project project) {
+        this.project = project;
+    }
 
     @TaskAction
-    void sayGreeting() {
-        System.out.printf("%s, %s\n", getMessage(), getRecipient());
+    void printProjectHierarchy() {
+        printProjectHierarchy(getProject());
+
+        getProject().subprojects(p -> {
+            printProjectHierarchy(p);
+        });
+    }
+
+    private static void printProjectHierarchy(Project p) {
+        System.out.printf("######## project: %s\n", p.getName());
+        ProjectConfig cfg = new ProjectConfig();
+
+        cfg.setArtifactId(p.getName()); // assume project name is also artifact ID
+        
+//        cfg.
+//        p.getDependencies().getModules()
+
+//        System.out.printf("  configurations: %s\n", p.getConfigurations());
+
+//        System.out.printf("  dependencies: %s\n", p.getDependencies().toString());
+//        System.out.printf("  properties: %s\n", p.getProperties().toString());
+//        System.out.printf("  artifacts: %s\n", p.getArtifacts().toString());
+//        System.out.printf("  repositories: %s\n", p.getRepositories().toString());
+//        System.out.printf("  version: %s\n", p.getVersion().toString());
+//        System.out.printf("  deps: %s\n", p.getDependencies().toString());
     }
 }
--- buildSrc/src/main/java/com/sourcegraph/gradle/MyPlugin.java
+++ buildSrc/src/main/java/com/sourcegraph/gradle/MyPlugin.java
@@ -5,9 +5,8 @@ import org.gradle.api.Project;
 
 public class MyPlugin implements Plugin<Project> {
     public void apply(Project project) {
-        project.getTasks().create("hello", Greeting.class, (task) -> {
-                task.setMessage("Hello");
-                task.setRecipient("World");
-            });
+        project.getTasks().create("javaconfig", Greeting.class, (task) -> {
+          task.setProject(project);
+        });
     }
 }
