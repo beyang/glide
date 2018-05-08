b23f139cf6d2d82d086b0a4de990b975cf7d6411
Beyang Liu <beyang@sourcegraph.com>
2018-05-07 19:24:19 -0700

    remove comments

--- buildSrc/src/main/java/com/sourcegraph/gradle/Greeting.java
+++ buildSrc/src/main/java/com/sourcegraph/gradle/Greeting.java
@@ -1,15 +1,12 @@
 package com.sourcegraph.gradle;
 
 import org.gradle.api.DefaultTask;
-import org.gradle.api.artifacts.ConfigurationContainer;
-import org.gradle.api.invocation.Gradle;
 import org.gradle.api.tasks.SourceSetContainer;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.Project;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -62,33 +59,6 @@ public class Greeting extends DefaultTask {
     void printProjectHierarchy() {
         Map<String, Object> pconfig = new HashMap<>();
         getProject().allprojects(project -> {
-//            System.out.printf("### %s:%s:%s\n", p.getGroup(), p.getName(), p.getVersion());
-//            p.getConfigurations().stream().forEach(c -> {
-//                System.out.println("### configurations");
-//                System.out.println(c.getAllDependencies().stream().collect(Collectors.toList()));
-//            });
-
-
-//            p.afterEvaluate(project -> {
-//                info.put("artifactId", project.getName());
-//                info.put("groupId", project.getGroup());
-//                info.put("version",  project.getVersion());
-//
-//                List<Map<String, Object>> deps = project.getConfigurations().stream()
-//                        .filter(c -> configWhitelist.contains(c.getName()))
-//                        .flatMap(c -> c.getAllDependencies().stream().map(d -> {
-//                            HashMap<String, Object> depInfo = new HashMap<>();
-//                            depInfo.put("group", d.getGroup());
-//                            depInfo.put("name", d.getName());
-//                            depInfo.put("version", d.getVersion());
-//                            return depInfo;
-//                        })).distinct().collect(Collectors.toList());
-//                info.put("dependencies", deps);
-//
-//                // TODO
-//                System.out.println("deps");
-//                System.out.println(deps);
-//            });
             Map<String, Object> info = new HashMap<>();
 
             info.put("artifactId", project.getName());
